f4c2ec46ff07873601ffe203a095808deab27c95
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-08-26 23:01:47 +0300

    h/w lesson 2 was added

--- lesson-2/main.go
+++ lesson-2/main.go
@@ -1,18 +1,9 @@
-package main
+//SquareOfRectangle is a function for calculation of Rectangle's square
+//
+//SquareOfRectangle returns int
+package pwd
 
-import (
-	"fmt"
-)
-
-func main() {
-	//It's a function for calculation of Rectangle's square
-	var a, b int
-	fmt.Println("Введите длины сторон прямоугольника")
-	fmt.Scanln(&a, &b)
-	fmt.Println(squareOfRectangle(a, b))
-}
-
-func squareOfRectangle(a, b int) int {
+func SquareOfRectangle(a, b int) int {
 	result := a * b
 	return result
 }
